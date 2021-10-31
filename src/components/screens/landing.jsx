36d804e6951ad50b8a689f6a583f2f5c2a64a71e
Manan Kakkar <54740965+kakkarmanan@users.noreply.github.com>
2021-10-31 16:25:47 +0530

    file structure

---
+++ src/components/screens/landing.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {  } from "react-bootstrap";
+
+function LandingPage(){
+        return (
+            <div>
+                <div className="header ">
+                    <div className="container-fluid-fluid">
+                        <nav className="navbar site-navbar offcanvas-active navbar-expand-lg px-0">
+                            <div className="beand-logo d-inline-block">
+                                <a href="google.com"><img src="./images/S4S-logos_transparent.png" alt="logo"></img></a>
+                                <div className="header-btns ml-aoto pr-2 ml-lg-9 d-none d-xs-flex"></div>
+                                <a className="btn btn-transparent-2 btn-small border-0 font-size-5 f0nt-weigth-normal text-periwinkle-gray focus-reset mr-6" href="google.com">Login</a>
+
+                                <a href="google.com" class="btn btn-2 btn-turquoise border border-turquoise font-size-5 text-firefly">Register</a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            // <div>
+            //     <Header/>
+            // </div>
+        );
+};
+
+export default LandingPage;
