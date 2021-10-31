5055b856e40928a0b43c58a6b57ca41ae46c0971
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-10-31 22:58:19 +0530

    login page sigunp page routing navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
+import {Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import RegisterPage from './components/screens/register';
+import LoginPage from './components/screens/login';
 import LandingPage from './components/screens/landing';
 import {} from 'react-bootstrap'
 function App() {
-  return <div>
-    <LandingPage/>
+  return (
+  <div>
+    <section>
+      <Route path="/" exact component = {LandingPage}/>
+    </section>
+    <section>
+      <Route path="/login" exact component = {LoginPage}/>
+    </section>
+    <section>
+      <Route path="/register" exact component = {RegisterPage}/>
+    </section>
   </div>
+  );
 }
 
 export default App;
--- src/components/screens/landing.jsx
+++ src/components/screens/landing.jsx
@@ -1,27 +1,21 @@
-import React from "react";
-import {  } from "react-bootstrap";
+    import React from "react";
+    import {  } from "react-bootstrap";
+    import {Link} from "react-router-dom";
 
-function LandingPage(){
-        return (
-            <div>
-                <div className="header ">
-                    <div className="container-fluid-fluid">
-                        <nav className="navbar site-navbar offcanvas-active navbar-expand-lg px-0">
-                            <div className="beand-logo d-inline-block">
-                                <a href="google.com"><img src="./images/S4S-logos_transparent.png" alt="logo"></img></a>
-                                <div className="header-btns ml-aoto pr-2 ml-lg-9 d-none d-xs-flex"></div>
-                                <a className="btn btn-transparent-2 btn-small border-0 font-size-5 f0nt-weigth-normal text-periwinkle-gray focus-reset mr-6" href="google.com">Login</a>
-
-                                <a href="google.com" class="btn btn-2 btn-turquoise border border-turquoise font-size-5 text-firefly">Register</a>
-                            </div>
-                        </nav>
-                    </div>
+    function LandingPage(){
+            return (
+                <div>
+                    <nav class="navbar navbar-dark bg-dark">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="#">s4s</a>
+                            <form class="d-flex">
+                                <Link to='/register' className ="button btn btn-outline-success me-2"> Signup</Link>
+                                <Link to='/login' className ="button btn btn-outline-success me-2"> Login</Link>
+                            </form>
+                        </div>
+                    </nav>
                 </div>
-            </div>
-            // <div>
-            //     <Header/>
-            // </div>
-        );
-};
+            );
+    };
 
-export default LandingPage;
+    export default LandingPage;
