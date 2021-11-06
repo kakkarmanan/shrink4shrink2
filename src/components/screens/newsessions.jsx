15571904d55d5fd4be233c406d855f0d50d959f6
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-11-06 18:20:46 +0530

    new session page made

--- src/components/screens/newsessions.jsx
+++ src/components/screens/newsessions.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Tilt from "react-parallax-tilt";
 
 class newsession extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          data:{
+              title:"",
+              date:"",
+              time:"",
+              u:JSON.parse(localStorage.getItem("user"))
+          }
+        }
+      }
   handleSignOut() {
     localStorage.removeItem("user");
   }
+  onChange = (e) => {
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value },
+    });
+    console.log(this.state.data);
+  };
+  onSchedule=(event)=>{
+    event.preventDefault();
+    if(this.state.data.title.length>0 && this.state.data.date.length>0 &&this.state.data.time.length>0){
+        fetch("http://localhost:3001/api/new_session",{
+            method:"post",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({
+                email:this.state.data.u.email,
+                title:this.state.data.title,
+                date:this.state.data.date,
+                time:this.state.data.time
+        }),
+    })
+    .then((response) => response.json())
+    .then((resp) => {
+        alert(resp);
+    });
+    }
+    else{
+        alert("Enter valid values");
+    }
+  };
   render() {
+    const {data} = this.state;
     return (
       <div>
         <nav className="navbar navbar-dark bg-dark">
@@ -66,9 +105,59 @@ class newsession extends React.Component {
             <div className="col main pt-5 mt-3">
               <h1 className="display-4 d-none d-sm-block">Schedule a session</h1>
               <div class="form-outline">
-                <input type="text" id="form16" className="form-control" data-mdb-showcounter="true" maxlength="150" style={{width:"50%"}} />
-                <label className="form-label" for="form16">Title of Appointment</label>
+              <div className="col-md-6 mb-4">
+                            <div className="form-outline">
+                              <input
+                                type="text"
+                                id="form3Example1m"
+                                className="form-control form-control-lg"
+                                name="title"
+                                value={data.title}
+                                onChange={this.onChange}
+                                required
+                                style={{width:"40%"}}
+                              />
+                              <label
+                                className="form-label"
+                                htmlFor="form3Example1m"
+                              >
+                                Title Of Appointment
+                              </label>
+                            </div>
+                          </div>
             <div className="form-helper">
+                          <div className="col-md-6 mb-4">
+            <div className="form-outline mb-4">
+                          <input
+                            type="date"
+                            id="form3Example9"
+                            className="form-control form-control-lg"
+                            name="date"
+                            value={data.date}
+                            onChange={this.onChange}
+                            style={{width:"40%"}}
+                            required
+                          />
+                          <label className="form-label" htmlFor="form3Example9">
+                            Date of Appointment
+                          </label>
+                        </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                        <div className="form-outline mb-4">
+  <input type="time" name="time" value={data.time} onChange={this.onChange} id="inputMDEx1" className="form-control" style={{width:"40%"}}/>
+  <label for="inputMDEx1">Choose your time</label>
+</div>
+                        </div>
+                        <div className="d-flex pt-3">
+                          <button
+                            type="button"
+                            className="btn btn-warning btn-lg ms-2 b1-color"
+                            onClick={this.onSchedule}
+                          >
+                            Schedule
+                          </button>
+                        </div>
             </div>
             </div>
             </div>
